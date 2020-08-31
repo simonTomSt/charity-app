3584d08a1e26812b8cea30595273a8b487095f17
Szymon Stepien <st.szymon2001@gmail.com>
2020-08-31 09:37:25 +0200

    Add initial files

---
+++ src/components/app/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LandingPage from "../landingPage/LandingPage";
+
+function App() {
+  return (
+    <>
+      <LandingPage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/landingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Opening from "./Opening/Opening";
+
+const LandingPage = () => {
+  return (
+    <>
+      <Opening />
+    </>
+  );
+};
+
+export default LandingPage;
