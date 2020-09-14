9a080470c1b714796d019da82e374142bd32ee73
Szymon Stepien <st.szymon2001@gmail.com>
2020-09-14 20:06:42 +0200

    Updates

---
+++ precache-manifest.cad50390319a8000b9e1f1a8291bcef5.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2957ce1e51165d7ceb69a3d4ec3c0ab2",
+    "url": "/charity-app/index.html"
+  },
+  {
+    "revision": "da81b936a7749ed5f287",
+    "url": "/charity-app/static/css/main.1504cc0b.chunk.css"
+  },
+  {
+    "revision": "b2cac653ee7209463b80",
+    "url": "/charity-app/static/js/2.38aa536f.chunk.js"
+  },
+  {
+    "revision": "388e667d555f121bdc71ab5c34de28f9",
+    "url": "/charity-app/static/js/2.38aa536f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da81b936a7749ed5f287",
+    "url": "/charity-app/static/js/main.60e1e061.chunk.js"
+  },
+  {
+    "revision": "566c0fe24b580c0a2af0",
+    "url": "/charity-app/static/js/runtime-main.16927bb1.js"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/charity-app/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "aab5aa4cf97893341cd5aed84c736cd5",
+    "url": "/charity-app/static/media/Background-Form.aab5aa4c.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/charity-app/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/charity-app/static/media/Facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "3bee335ab47afd8b714411f4841f0185",
+    "url": "/charity-app/static/media/Form-Hero-Image.3bee335a.jpg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/charity-app/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/charity-app/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/charity-app/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/charity-app/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/charity-app/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "c470ec0c56e643f8ee1898ea962e753a",
+    "url": "/charity-app/static/media/Icon-Arrow-Down.c470ec0c.svg"
+  },
+  {
+    "revision": "9b7f6ef4fe770eee646ea281f111b15d",
+    "url": "/charity-app/static/media/Icon-Arrow-Up.9b7f6ef4.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/charity-app/static/media/Instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/charity-app/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/charity-app/static/media/Signature.e54cfcfc.svg"
+  },
+  {
+    "revision": "99f2e620a9f4302f9861282162231969",
+    "url": "/charity-app/static/media/columns.99f2e620.png"
+  }
+]);
